6111deb6c276d74f2a558045eb5578952d97c2c0
hxq <495157297@qq.com>
2016-11-08 10:45:54 +0800

    gradient decent

---
+++ linear_regression/gradien_decent.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+iris = load_iris()
+X= iris.data[:-1,:]
+y = iris.target[:-1]
+logistic = LogisticRegression()
+logistic.fit(X,y)
+print('Predicted class %s, real class %s' % (logistic.predict(iris.data[-1,:]),iris.target[-1]))
+print('Probabilities for each class from 0 to 2: %s' % logistic.predict_proba(iris.data[-1,:]))
